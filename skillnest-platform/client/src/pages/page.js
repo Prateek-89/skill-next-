26815dbb9a67438c6e88f72b42a40520b22fed2b
prateek kumar <chaturvediprateek474@gmail.com>
2026-02-11 03:36:26 +0530

    add home page

---
+++ skillnest-platform/client/src/pages/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <main style={{ padding: 40 }}>
+        <h1>Deployment Successful 🚀</h1>
+        <p>Your Next.js app is live on Vercel.</p>
+      </main>
+    );
+  }
+  
